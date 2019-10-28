a7aa7ad8bfa44f322a21d126098befe2fbfeb6d6
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-10-29 00:07:00 +0100

    add town to address

--- Sources/NominatimSwift/Address.swift
+++ Sources/NominatimSwift/Address.swift
@@ -13,6 +13,7 @@ public struct Address: Decodable {
     public var suburb: String?
     public var cityDistrict: String?
     public var city: String?
+    public var town: String?
     public var county: String?
     public var state: String?
     public var region: String?
@@ -22,6 +23,6 @@ public struct Address: Decodable {
 
     enum CodingKeys: String, CodingKey {
         case houseNumber = "house_number", road, suburb, cityDistrict = "city_district"
-        case city, county, state, region, postcode, country, countryCode = "country_code"
+        case city, town, county, state, region, postcode, country, countryCode = "country_code"
     }
 }
